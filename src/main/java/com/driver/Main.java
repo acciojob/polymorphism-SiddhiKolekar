5574e51bf344219d609a0621b832e0d3b0cad53b
Siddhi <siddhikolekar2018@gmaiil.com>
2023-11-21 20:18:18 +0530

    Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,7 @@
 package com.driver;
 
 public class Main {
-    public static void main(String args[]){
-        Product p = new Product();
-        int ans = p.Product(10,20);
-        System.out.println(ans);
-        int ans1 = p.Product(10,20,30);
-        System.out.println(ans1);
-        double ans2 = p.Product(10.0,20.0);
-        System.out.println(ans2);
-       // System.out.println(ans+" "+ans1+" "+ans2);
-    }
+
     public static class Product{
     public int Product(int x, int y){
         return (x*y);
@@ -21,5 +12,20 @@ public class Main {
     public double Product(double x, double y){
         return (x*y);
     }
+    }
+    public static void main(String args[]){
+        Product p = new Product();
+
+        //Task 3: create a method of following defination
+        int ans = p.Product(10,20);
+        System.out.println(ans);
+
+        //Task 4: Create an Overload method product of following defination
+        int ans1 = p.Product(10,20,30);
+        System.out.println(ans1);
+
+        //Task 5: Create an Overload method product of following defination
+        double ans2 = p.Product(10.0,20.0);
+        System.out.println(ans2);
     }
 }
